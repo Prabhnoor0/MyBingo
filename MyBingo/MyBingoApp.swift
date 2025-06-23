b8738eb0941e52f4e3d06197b37be45c970c128e
Prabhnoor0 <prabhnooorkaur11@gmail.com>
2025-06-23 14:39:28 +0530

    Initial Commit

---
+++ MyBingo/MyBingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBingoApp.swift
+//  MyBingo
+//
+//  Created by Prabhnoor Kaur on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
