b545fcc652a58c0f8454c54bf03cac5d7f32c08a
Prabhnoor0 <prabhnooorkaur11@gmail.com>
2025-06-23 22:21:15 +0530

    Completed Main Game Logic

--- MyBingo/GameLogic/CoreLogic.swift
+++ MyBingo/GameLogic/CoreLogic.swift
@@ -8,19 +8,45 @@
 import Foundation
 var playerBoardNumbers:[Int]=Array(1...25).shuffled()     //numbers written on player's board
 var aiBoardNumbers:[Int]=Array(1...25).shuffled()     //numbers written on ai's board
-var markedNumbers:Set<Int>=[]     //numbers which have already been marked
+var markedNumbers:[Int]=[]     //numbers which have already been marked
 var aiMarkedPositions: Set<Int>=[]
 var playerMarkedPositions: Set<Int>=[]
-var aiLines=0
-var playerLines=0
-var winPositions:[[Int]]=[[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19],[20,21,22,23,24],[0,5,10,15,20],[1,6,11,16,21],[2,7,12,17,22],[3,8,13,18,23],[4,9,14,19,24],[0,6,12,18,24],[4,10,16,22,24]]
+var aiWin=false
+var playerWin=false
+var gameEnded=false
+var isPlayerTurn=true
+var winPositions:[[Int]]=[[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19],[20,21,22,23,24],[0,5,10,15,20],[1,6,11,16,21],[2,7,12,17,22],[3,8,13,18,23],[4,9,14,19,24],[0,6,12,18,24],[4,8,12,16,20]]
 
+func aiChooseNumber(){
+    var aiRemainingNumbers:[Int]=aiBoardNumbers.filter{!markedNumbers.contains($0)}
+    if aiRemainingNumbers.isEmpty {
+        return
+    }
+    if let aiNumber = aiRemainingNumbers.randomElement() {
+        givenNo(number: aiNumber)
+    }
 
-func givenNo(number:Int){                   //marks the no for all users
-    var aiWin=false
-    var playerWin=false
+}
+
+func playerClickedNumber(numberChosen:Int){
+    if(!gameEnded){
+        if(isPlayerTurn){
+            givenNo(number:numberChosen)
+            isPlayerTurn=false
+            if(!gameEnded){
+                aiChooseNumber()
+                isPlayerTurn=true
+            }
+        }
+    }
+}
+
+func givenNo(number:Int){                       //marks the no for all users
+    if(gameEnded){
+        return
+    }
     if(!markedNumbers.contains(number)){        //checks if number is already marked just in case
-        markedNumbers.insert(number)
+        markedNumbers.append(number)
     if let index = aiBoardNumbers.firstIndex(of: number) {     //checks index of no in ai board
             aiMarkedPositions.insert(index)        //adds position of the number in ai board
         aiWin = checkWin(positions: aiMarkedPositions)
@@ -31,6 +57,7 @@ func givenNo(number:Int){                   //marks the no for all users
         playerWin = checkWin(positions: playerMarkedPositions)
         }
         if(aiWin||playerWin){
+            gameEnded=true
             if(aiWin&&playerWin){
                 print("It's a tie\n Game Ends")
             }
@@ -55,6 +82,10 @@ func newGame()->Void{                               //restarts the game
     markedNumbers.removeAll()       //empties the marked numbers list as game is restarted
     aiMarkedPositions.removeAll()
     playerMarkedPositions.removeAll()
+    aiWin=false
+    playerWin=false
+    gameEnded=false
+    isPlayerTurn = true
 }
 
 func checkWin(positions: Set<Int>)->Bool{
@@ -69,6 +100,9 @@ func checkWin(positions: Set<Int>)->Bool{
         }
         if(isPresent){
             count+=1
+            if(count==5){
+                return true
+            }
         }
         
     }
